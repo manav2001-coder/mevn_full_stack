d2fc80028dd5567e7aff8f30475606d3ababf245
manav2001-coder <immanav2001@gmail.com>
2023-09-13 16:36:24 +0530

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,61 @@
+import express from "express"
+import morgan from "morgan"
+import { userModel } from "./models/index.js"
+
+
+//Initialization
+const app = express()
+
+
+//Middleware
+app.use(express.json())
+app.use(morgan("tiny"))
+
+//Session Single Source Of Truth
+let session = {username:""}
+
+//Function that returns user having a specific username
+function getUser(username){
+    return userModel.findOne({username:username})
+}
+
+// listening to port 3000
+app.listen(3000,()=>{
+    console.log("Hey i am the server")
+})
+
+//Login API
+app.post("/api/login",(req,res) => {
+    let User = getUser(req.body.username)
+    User.then((user) => {
+        if(user.password === req.body.password){
+         session.username=user.username
+         res.send({ok:true,session:session})
+        }
+    }).catch((err) => res.send({ok:false,error:err}))
+
+})
+
+//Add User API
+app.post("/api/addUser",(req,res) => {
+    let user = new userModel({username:req.body.username,password:req.body.password,roll_no:1})
+    user.save()
+    res.send({ok:true})
+})
+
+//Navigation Handler API
+app.get("/api/auth",(req,res) => {
+    if(session.username)
+      res.send({ok:true,session:session})
+    else{
+        res.send({ok:false})
+    }
+    
+      
+})
+
+//Logout API
+app.get("/api/logout",(req,res) => {
+     session={};
+     res.send({ok:true})
+})
---
+++ backend/models/index.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+import mongooseEncryption from 'mongoose-encryption'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+mongoose.connect("mongodb://localhost:27017/demo")
+
+//Schemas
+const userSchema = new mongoose.Schema({
+    username:String,
+    password:String,
+    roll_no:Number
+})
+
+userSchema.plugin(mongooseEncryption,{secret:process.env.SECRET,encryptedFields:["password"]})
+
+
+//Models
+const userModel = new mongoose.model("user",userSchema)
+
+
+export{ userModel }
